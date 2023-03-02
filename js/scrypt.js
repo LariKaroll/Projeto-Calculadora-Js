33b7211a8c5c9c23a6450dc0ef13163e3f9c29e6
LariKaroll <larikarol108@gmail.com>
2023-03-02 03:45:40 -0300

    90%

---
+++ js/scrypt.js
@@ -0,0 +1,64 @@
+var imprimirCalc = document.querySelector('#telaCalc')
+var numeroZero = document.querySelector('#num0');
+var numeroUm = document.querySelector('#num1');
+var numeroDois = document.querySelector('#num2');
+var numeroTres = document.querySelector('#num3');
+var numeroQuatro = document.querySelector('#num4');
+var numeroCinco = document.querySelector('#num5');
+var numeroSeis = document.querySelector('#num6');
+var numeroSete = document.querySelector('#num7');
+var numeroOito = document.querySelector('#num8');
+var numeroNove = document.querySelector('#num9');
+
+numeroZero.onclick = function imprimirNumeroCalcNumZero() {
+    numeroZero = 0;
+    imprimirCalc.innerHTML = numeroZero;
+}
+
+numeroUm.onclick = function imprimirNumeroCalcNumUm() {
+    numeroUm = 1;
+    imprimirCalc.innerHTML = numeroUm;
+}
+
+numeroDois.onclick = function imprimirNumeroCalcNumDois() {
+    numeroDois = 2;
+    imprimirCalc.innerHTML = numeroDois;
+}
+
+numeroTres.onclick = function imprimirNumeroCalcNumTres(){
+    numeroTres = 3;
+    imprimirCalc.innerHTML = numeroTres;
+}
+
+numeroQuatro.onclick = function imprimirNumeroCalcNumQuatro(){
+
+    numeroQuatro = 4;
+    imprimirCalc.innerHTML = numeroQuatro;
+}
+
+numeroCinco.onclick = function imprimirNumeroCalcNumCinco(){
+    numeroCinco = 5;
+    imprimirCalc.innerHTML = numeroCinco;
+}
+
+numeroSeis.onclick = function imprimirNumeroCalcNumSeis(){
+    numeroSeis = 6;
+    imprimirCalc.innerHTML = numeroSeis;
+}
+
+numeroSete.onclick = function imprimirNumeroCalcNumSete(){
+    numeroSete = 7;
+    imprimirCalc.innerHTML = numeroSete;
+}
+
+numeroOito.onclick = function imprimirNumeroCalcNumOito(){
+    numeroOito = 8;
+    imprimirCalc.innerHTML = numeroOito;
+}
+
+numeroNove.onclick = function imprimirNumeroCalcNumNove(){
+    numeroNove = 9;
+    imprimirCalc.innerHTML = numeroNove;
+}
+
+if()
